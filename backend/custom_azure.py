12a39ec2d59ac8573a2167852c891eece8eac1ff
Anas-Khan93 <anas_mgs@yahoo.com>
2024-08-15 23:03:13 +0500

    Added CRUD PRODUCT and ADDED PICTURE UPLOADING TO AZURE BLOB STORAGE

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -1,7 +1,7 @@
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    aacount_name= 'ecomimage'
-    account_key= '1mu85ovEvWpOJySN4yZgjhIKyDXc3cDQ58NjYxzOSPKqiXQYGGKiXbYwz0GzUEM9hs1KTUzVFsnb+AStKIqzyQ=='
+    account_name= os.getenv("AZURE_ACCOUNT_NAME", default="")
+    account_key= os.getenv("AZURE_ACCOUNT_KEY", default="")
     azure_container= 'media'
     expiration_secs= None
