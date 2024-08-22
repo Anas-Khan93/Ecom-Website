af5bbfdbe239abfff6794dee03341ed08d6a5d67
Anas-Khan93 <anas_mgs@yahoo.com>
2024-08-22 13:17:12 +0500

    UPDATED SETTINGS FILE BEFORE DEPLOYING

--- ai_interviewer/custom_azure.py
+++ ai_interviewer/custom_azure.py
@@ -2,7 +2,7 @@ import os
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name= os.getenv("AZURE_ACCOUNT_NAME")
-    account_key= os.getenv("AZURE_ACCOUNT_KEY")
-    azure_container= 'media'
+    account_name= env("AZURE_ACCOUNT_NAME")
+    account_key= env("AZURE_ACCOUNT_KEY")
+    azure_container= env('AZURE_CONTAINER')
     expiration_secs= None
