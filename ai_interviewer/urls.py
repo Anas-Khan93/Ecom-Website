667070900f4cd9094f091d0ef95faa6bd13b6a48
Anas-Khan93 <anas_mgs@yahoo.com>
2024-08-23 12:47:40 +0500

    Added a path for root url

--- ai_interviewer/urls.py
+++ ai_interviewer/urls.py
@@ -1,11 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("WELCOME TO THE HOME PAGE")
+
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('api/', include("ai_appinterviewer.api.urls")),
-    
+    path('', home, name='home'), # this line handles the root path
     
 ]
