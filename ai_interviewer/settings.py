d7ec12cf51b96418297c8115824be3112dd80c7f
Anas-Khan93 <anas_mgs@yahoo.com>
2024-08-23 22:04:58 +0500

    added all hosts as allowed

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -39,7 +39,10 @@ environs.Env.read_env()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mytest-ecom-app-ghcdcpbugddhe6gz.eastus2-01.azurewebsites.net', '127.0.0.1', '169.254.130.7']
+# ALLOWED_HOSTS = ['mytest-ecom-app-ghcdcpbugddhe6gz.eastus2-01.azurewebsites.net', '127.0.0.1', '169.254.130.7']
+
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
