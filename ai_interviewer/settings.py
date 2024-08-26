8e011ab44ff592b1914348a95a12c759ee92bb05
Anas-Khan93 <anas_mgs@yahoo.com>
2024-08-26 14:06:02 +0500

    IMIMIM

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -93,9 +93,9 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
